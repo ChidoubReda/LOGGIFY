430218f7e2e71960a98357e934960d08db189fb4
ChidoubReda <127399093+ChidoubReda@users.noreply.github.com>
2024-05-31 18:44:28 +0200

    Create config_BACKUP.sh

---
+++ config_BACKUP.sh
@@ -0,0 +1,11 @@
+# Interval duration in seconds
+INTERVAL=1
+
+# Path of logfile
+LOG_LOCATION="./"
+
+# Email to send report
+EMAIL="your@mail.com"
+
+# Target date in format YYYY-MM-DD HH:MM:SS
+TARGET_DATE=""
