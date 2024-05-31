d710ecc10020894dfea6802d352b0ccf9c6e650a
ChidoubReda <127399093+ChidoubReda@users.noreply.github.com>
2024-05-31 18:43:09 +0200

    Create style.sh

---
+++ style.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Text color variables
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+
+# Text styles
+BOLD='\033[1m'
+DIM='\033[2m'
+UNDERLINE='\033[4m'
+BLINK='\033[5m'
+REVERSE='\033[7m'
+HIDDEN='\033[8m'
+
+# Reset text format
+NC='\033[0m' # No Color
